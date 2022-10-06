9bbf3f5fc4ea9dee3725d0e94bda5257895a9cc9
john3c <john3corrigan@gmail.com>
2022-10-05 20:04:32 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Corrigan, John on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
