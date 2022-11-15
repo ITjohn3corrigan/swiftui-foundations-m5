60b23d627295bb342973e2e36779b236f90ce1cc
john3c <john3corrigan@gmail.com>
2022-11-15 15:17:04 -0500

    resource add style and data

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init(){
+      getLocalData()
+    }
+    
+    func getLocalData() {
+        //parse the local json file
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            //get a url to the json file
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            let modules  =  try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            //assign parsed modules to module property
+            self.modules = modules
+            
+        } catch{
+            print("couldn't parse local data")
+        }
+        //parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        do{
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        }catch{
+            print("couldn't parse style data")
+        }
+
+    }
 }
